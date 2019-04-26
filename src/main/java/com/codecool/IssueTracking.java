7e35e049ca7da234112fb1654ef770659d895103
Horinka Peter <horinka.peter@gmail.com>
2019-04-26 09:36:03 +0200

    Refactor: CDD functinos if/else has seperated to 2 functions

--- src/main/java/com/codecool/IssueTracking.java
+++ src/main/java/com/codecool/IssueTracking.java
@@ -2,30 +2,36 @@ package com.codecool;
 
 public class IssueTracking {
 
+    final int WORKHOURS = 8;
+    final int WORKSTART = 9;
+    final int WORKEND = 17;
+
     public String calculateDueDate(Date submitDate, int turnAroundTime) {
         if (submitDate.hour > 23 || submitDate.minute > 59){
             throw new IllegalArgumentException("");
         }
-
-        final int WORKHOURS = 8;
-        final int WORKSTART = 9;
-        final int WORKEND = 17;
-
-        if (turnAroundTime % WORKHOURS == 0) { //todo
-            for (int i = 0; i < turnAroundTime / WORKHOURS; i++)
-                submitDate.day = submitDate.day.next();
+        if (turnAroundTime % WORKHOURS == 0) {
+            turnAroundTimeIsExactlyOneOrMoreWorkday(submitDate, turnAroundTime);
         } else {
-            for (int i = 0; i < turnAroundTime / WORKHOURS; i++) {
-                submitDate.day = submitDate.day.next();
-            }
-
-            submitDate.hour += turnAroundTime % WORKHOURS;
-            if (submitDate.hour >= WORKEND) {
-                int remainingTime = submitDate.hour - WORKEND;
-                submitDate.day = submitDate.day.next();
-                submitDate.hour = WORKSTART + remainingTime;
-            }
+            turnAroundTimeModuloWorkhoursIsNotZero(submitDate, turnAroundTime);
         }
         return submitDate.toString();
+    }
+
+    private void turnAroundTimeIsExactlyOneOrMoreWorkday(Date submitDate, int turnAroundTime){
+        for (int i = 0; i < turnAroundTime / WORKHOURS; i++)
+            submitDate.day = submitDate.day.next();
+    }
+
+    private void turnAroundTimeModuloWorkhoursIsNotZero(Date submitDate, int turnAroundTime){
+        for (int i = 0; i < turnAroundTime / WORKHOURS; i++) {
+            submitDate.day = submitDate.day.next();
+        }
+        submitDate.hour += turnAroundTime % WORKHOURS;
+        if (submitDate.hour >= WORKEND) {
+            int remainingTime = submitDate.hour - WORKEND;
+            submitDate.day = submitDate.day.next();
+            submitDate.hour = WORKSTART + remainingTime;
+        }
     }
 }
